de060b64d72480c98fc1781231e1dc2b670575a0
AmitRanjanvth <bittuqqqq4@gmail.com>
2025-12-24 19:49:47 +0530

    offers section added to web

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -3,12 +3,14 @@ import Hero from '../Components/Hero'
 import Testimonial from '../Components/Testimonial'
 import Tentcard from '../Components/Tentcard'
 import FeaturedDestination from '../Components/FeaturedDestination'
+import ExclusiveOffers from '../Components/ExclusiveOffers'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <FeaturedDestination />
+      <ExclusiveOffers />
       <Testimonial />
 
     </div>
