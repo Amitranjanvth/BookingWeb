4c425c012830537947421ef1d3d5f15490f55f99
AmitRanjanvth <bittuqqqq4@gmail.com>
2026-01-18 15:36:53 +0530

    databse connection

--- server/Serever.js
+++ server/Serever.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
+import connectDB from './config/database.js';
+
+connectDB();
 
 const app = express();
 app.use(cors());
