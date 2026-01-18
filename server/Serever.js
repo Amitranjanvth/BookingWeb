517d7847d2d61669c7f42045fb251f0014ea085f
AmitRanjanvth <bittuqqqq4@gmail.com>
2026-01-18 15:14:51 +0530

    Express app setup

---
+++ server/Serever.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import "dotenv/config";
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to you in this World!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
