48f23f03d178c022bcdbd3ef10965178307f28b5
AmitRanjanvth <bittuqqqq4@gmail.com>
2026-01-19 17:46:41 +0530

    models added

--- server/config/database.js
+++ server/config/database.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        mongoose.connection.on('connected', () => {
+            mongoose.connection.on('connected', () => {
             console.log('MongoDB connected successfully');
-        });
+            });
         await mongoose.connect(`${process.env.MONGODB_URI}/eventwallah` )
     } catch (error) {
         console.error("Error connecting to MongoDB:", error.message);
